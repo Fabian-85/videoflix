529a755aeb71c7007ca6aafd20944a82e6091c7d
Fabian-85 <fabian.maier0805@hotmail.com>
2025-10-01 13:46:29 +0200

    feat: create a activation-token for activate a account in registration view and create a activation view

--- auth_app/api/urls.py
+++ auth_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RegistrationView, TestView
+from .views import RegistrationView, ActivateAccountView, TestView
  
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
+    path("activate/<uidb64>/<token>/", ActivateAccountView.as_view(), name="activate_account"),
     path('token-test/', TestView.as_view(), name='test')   
 ]
--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -3,10 +3,17 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
- 
- 
+from .tokens import account_activation_token
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
 class RegistrationView(APIView):
-    
+
     permission_classes = [AllowAny]
 
     def post(self, request):
@@ -14,15 +21,41 @@ class RegistrationView(APIView):
         data = {}
         if serializer.is_valid():
             saved_account = serializer.save()
+            uidb64 = urlsafe_base64_encode(force_bytes(saved_account.pk))
+            token = account_activation_token.make_token(saved_account)
+            activate_path = reverse("activate_account", kwargs={"uidb64": uidb64, "token": token})
+            activate_url = request.build_absolute_uri(activate_path)
             data = {
-                'username': saved_account.username,
-                'email': saved_account.email,
-                'user_id': saved_account.pk
+                'user': {
+                    'user_id': saved_account.pk,
+                    'email': saved_account.email,
+                    'uidb64':activate_url
+                },
+                'token':token
             }
 
             return Response(data)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ActivateAccountView(APIView):
+    dpermission_classes=[AllowAny]
+
+    def get(self, request, uidb64, token):
+        try:
+         uid = force_str(urlsafe_base64_decode(uidb64))
+         user = User.objects.get(pk=uid)
+        except:
+            return Response({"message": "User not found"},status=status.HTTP_400_BAD_REQUEST)
+        is_token_valid = account_activation_token.check_token(user, token)
+        if is_token_valid:
+            user.is_active = True
+            user.save()
+            print("True")
+            return Response({"message": "Account successfully activated"})
+        else:
+            return Response({"message": "User Account is active"})
 
 
 class TestView(APIView):
