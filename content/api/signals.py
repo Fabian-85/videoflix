6c2ea12a79cd2331abd6f1e97fbedd73ab36618e
Fabian-85 <fabian.maier0805@hotmail.com>
2025-10-23 13:16:16 +0200

    feat(video): rebuild HLS variants and clean up old files on video update

--- content/api/signals.py
+++ content/api/signals.py
@@ -16,7 +16,22 @@ def enqueue_hls_jobs_on_create(sender, instance, created, **kwargs):
     else:
         print("updated")
 
+@receiver(pre_save, sender=Video)
+def enqueue_hls_jobs(sender, instance, **kwargs): 
+    is_create = instance._state.adding
+    if instance.pk and not is_create:
+        old_instance = Video.objects.get(pk=instance.pk)
+        if old_instance.video_file != instance.video_file:
+            delete_file_if_exists(old_instance.video_file)
+            delete_hls_directory_for_video(old_instance.id)
+            queue = django_rq.get_queue('default', autocommit=True)
+            queue.enqueue(build_hls_variant, instance.pk, '480')
+            queue.enqueue(build_hls_variant, instance.pk, '720')
+            queue.enqueue(build_hls_variant, instance.pk, '1080')
+        if old_instance.thumbnail_url != instance.thumbnail_url:
+            delete_file_if_exists(old_instance.thumbnail_url)
 
+            
 @receiver(post_delete, sender=Video)
 def cleanup_video_files_on_delete(sender, instance, **kwargs):
     delete_file_if_exists(instance.thumbnail_url)
