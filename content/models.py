1c9a32d18bee17354d5a0fdc2f421c65ed19833e
Fabian-85 <fabian.maier0805@hotmail.com>
2025-10-23 13:06:12 +0200

    feat(video-model): add Validation for image and video files

--- content/models.py
+++ content/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
 
 
@@ -7,15 +8,17 @@ VIDEO_CATEGORY_CHOICES = {
     ('comedy', 'Comedy')
 }
 
+VIDEO_EXTS = ["mp4", "mov", "mkv", "webm", "avi"]
+IMAGE_EXTS = ["jpg", "jpeg", "png", "gif", "webp"]
 class Video(models.Model):
 
     title = models.CharField(max_length=255)
     description = models.TextField()
     category = models.CharField(choices=VIDEO_CATEGORY_CHOICES)
-    thumbnail_url = models.FileField(upload_to='thumbnails/')
+    thumbnail_url = models.FileField(upload_to='thumbnails/', validators=[FileExtensionValidator(allowed_extensions=IMAGE_EXTS)])
     created_at = models.DateTimeField(auto_now_add=True)
 
-    video_file = models.FileField(upload_to='videos/', null=True)
+    video_file = models.FileField(upload_to='videos/',  validators=[FileExtensionValidator(allowed_extensions=VIDEO_EXTS)], null=True)
 
     hls_480p = models.FileField(upload_to='hls/',  null=True, blank=True,editable=False)
     hls_720p = models.FileField(upload_to='hls/',  null=True, blank=True,editable=False)
