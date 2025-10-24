10bec76b36a075c0dd3aab558ef5797e926a5045
Fabian-85 <fabian.maier0805@hotmail.com>
2025-10-24 10:22:25 +0200

    refactor(signals):trigger  hls-jobs for change video file in post_save triggern + flag is_change_video_file in pre_save setzen

--- content/api/signals.py
+++ content/api/signals.py
@@ -9,38 +9,38 @@ from content.api.functions import build_hls_variant, delete_file_if_exists, dele
 def enqueue_hls_jobs_on_create(sender, instance, created, **kwargs):
 
     """
-    Enqueue HLS variant in 480p, 720p and 1080p building jobs when a new Video is created.
+    Enqueue HLS variant in 480p, 720p and 1080p building jobs 
+    when a Video is created or the video file is updated.
     """
 
-    if created:
-        print("created")
+    if created or instance.is_change_video_file:
         queue = django_rq.get_queue('default', autocommit=True)
         queue.enqueue(build_hls_variant, instance.pk, '480')
         queue.enqueue(build_hls_variant, instance.pk, '720')
         queue.enqueue(build_hls_variant, instance.pk, '1080')
-    else:
-        print("updated")
+    
+    if instance.is_change_video_file:
+        instance.is_change_video_file = False
+        instance.save(update_fields=['is_change_video_file'])
 
 
 @receiver(pre_save, sender=Video)
 def enqueue_hls_jobs_and_delete_old_files_on_update(sender, instance, **kwargs): 
 
     """
-    Enqueue HLS variant in 480p, 720p and 1080p building jobs when a Video's video_file is updated.
     Delete old video and HLS files if they are replaced.
+    Set is_change_video_file flag to True if video file is changed.
+    This flag is used to trigger HLS building in post_save signal.
     Also delete old thumbnail if it is replaced.
     """
-
-    print("pre_save signal triggered")
-    if instance.pk:
+     
+    is_create = instance._state.adding
+    if instance.pk and not is_create:
         old_instance = Video.objects.get(pk=instance.pk)
         if old_instance.video_file != instance.video_file:
             delete_file_if_exists(old_instance.video_file)
             delete_hls_directory_for_video(old_instance.id)
-            queue = django_rq.get_queue('default', autocommit=True)
-            queue.enqueue(build_hls_variant, instance.pk, '480')
-            queue.enqueue(build_hls_variant, instance.pk, '720')
-            queue.enqueue(build_hls_variant, instance.pk, '1080')
+            setattr(instance, 'is_change_video_file', True)
         if old_instance.thumbnail_url != instance.thumbnail_url:
             delete_file_if_exists(old_instance.thumbnail_url)
 
