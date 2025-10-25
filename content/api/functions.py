43cc16904cbc6afbb825d9839567cd5b2112fc6a
Fabian-85 <fabian.maier0805@hotmail.com>
2025-10-25 11:12:07 +0200

    feat(video-signals): transcoding video in 360p, 720p and 1080p (not temporary)

--- content/api/functions.py
+++ content/api/functions.py
@@ -19,6 +19,8 @@ def convert_to_hdx(source, target, resolution):
         resolution (str): Target resolution (e.g., 'hd480', 'hd720', 'hd1080').
     """
 
+    out_dir = os.path.dirname(target)
+    os.makedirs(out_dir, exist_ok=True)
     cmd = f'ffmpeg -i "{source}" -s {resolution} -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
     run = subprocess.run(cmd, capture_output=True, shell=True)
 
@@ -36,7 +38,7 @@ def build_hls_playlist(source, target):
     out_dir = os.path.dirname(target)
     os.makedirs(out_dir, exist_ok=True)
     cmd = 'ffmpeg -i "{}" -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -hls_flags independent_segments -hls_segment_filename "seg_%03d.ts" -f hls "{}"'.format(
-        source, target)
+       source, target)
     run = subprocess.run(cmd, capture_output=True, cwd=out_dir, shell=True)
 
 
@@ -49,24 +51,23 @@ def build_hls_variant(video_id, resolution):
     Args:
         video_id (int): ID of the Video instance.
         resolution (str): Target resolution (e.g., '480', '720', '1080').
-
     """
 
     video = Video.objects.get(pk=video_id)
     src_path = video.video_file.path
     basename = Path(src_path).stem
-    with tempfile.TemporaryDirectory() as tmp:
-        out = os.path.join(tmp, f"{basename}_{resolution}p.mp4")
-        convert_to_hdx(src_path, out, f"hd{resolution}")
 
-        hls_dir = os.path.join(settings.MEDIA_ROOT, 'hls',
+    out = os.path.join(settings.MEDIA_ROOT, 'tmp', f'video_{video_id}', f"{basename}_{resolution}p.mp4")
+    convert_to_hdx(src_path, out, f"hd{resolution}")
+        
+    hls_dir = os.path.join(settings.MEDIA_ROOT, 'hls',
                                f'video_{video.id}', f'{resolution}p', f"{basename}_{resolution}p.m3u8")
-        build_hls_playlist(out, hls_dir)
+    build_hls_playlist(out, hls_dir)
 
-        rel = os.path.relpath(hls_dir, settings.MEDIA_ROOT)
-        field_name = f'hls_{resolution}p'
-        setattr(video, field_name, rel)
-        video.save(update_fields=[field_name])
+    rel = os.path.relpath(hls_dir, settings.MEDIA_ROOT)
+    field_name = f'hls_{resolution}p'
+    setattr(video, field_name, rel)
+    video.save(update_fields=[field_name])
 
 
 def delete_file_if_exists(url):
